1b0635fef627f7a253e5435d95645bdecfc02d97
bozhidarkg <bozhidar.georgiev6@gmail.com>
2019-06-24 16:31:16 +0300

    Add files via upload

---
+++ Internship/BasketballPlayers/BasketballPlayers/Player.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasketballPlayers
+{
+    class Player
+    {
+       private string name;
+       private short playingSince;
+       private string position;
+       private short rating;
+
+        public String Name {
+            get { return this.name; }
+            set { this.name = value; }
+
+        }
+
+        public short PlayingSince {
+            get { return this.playingSince; }
+            set { this.playingSince = value; }
+        }
+
+        public String Position {
+            get { return this.position; }
+            set { this.position = value; }
+        }
+        
+
+        public short Rating {
+            get { return this.rating; }
+            set { this.rating = value; }
+        }
+        public  Player(string name,short playingSince, string position,short rating) {
+            this.name = name;
+            this.playingSince = playingSince;
+            this.position = position;
+            this.rating = rating;
+        }
+
+        public Player() { }
+
+    }
+}
---
+++ Internship/BasketballPlayers/BasketballPlayers/Program.cs
@@ -0,0 +1,44 @@
+﻿using Nancy.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace BasketballPlayers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter the path to the JSON file containing the players: ");
+            String jsonPath = Console.ReadLine();
+            Console.Write("Please enter maximum number of years the player has played in the league to qualify: ");
+            short numberOfYears = short.Parse(Console.ReadLine());
+            Console.Write("Please enter a number representing the minimum rating the player should have to qualify: ");
+            short minRating = short.Parse(Console.ReadLine());
+            Console.Write("Please enter the  path to the CSV  file: ");
+            String pathToFile = Console.ReadLine();
+            Console.WriteLine();
+
+            String rawJson = rawJson = File.ReadAllText(jsonPath);
+            List<Player> players = JsonConvert.DeserializeObject<List<Player>>(rawJson);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Name,Rating" + System.Environment.NewLine);
+
+            players = players.OrderByDescending(p => p.Rating).ToList();
+            foreach (Player pl in players)
+            {
+                if (pl.Rating >= minRating && (2019 - pl.PlayingSince) <= numberOfYears)
+                {
+                    sb.Append(pl.Name + ", " + pl.Rating + System.Environment.NewLine);
+                }
+            }
+
+            File.WriteAllText(pathToFile, sb.ToString());
+        }
+    }
+}
